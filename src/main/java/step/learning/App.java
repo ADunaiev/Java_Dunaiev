15072f8987705414501f199eb47e879f658871d1
ADunaiev <111633434+ADunaiev@users.noreply.github.com>
2024-03-02 17:41:45 +0200

    First commit

--- src/main/java/step/learning/App.java
+++ src/main/java/step/learning/App.java
@@ -4,10 +4,35 @@ package step.learning;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        new Intro().demo();
     }
 }
+
+/*
+Java - Птатформна (транслююча) мсва 4-го покоління з ООП парадигмою.
+- платформа JRE (JVM)
+- вихідний код - file.java
+- виконавчий (проміжний) код file.class [> java.exe file.class ]
+- JDK - інструментарій розробника (компілятор + бібліотеки)
+    javac.exe file.java --> file.class
+- IDE: Intellij Idea, Netbeans, Eclipse, ...
+- бібліотеки коду - lib.jar (аналог DLL)
+
+Сильно прив'язка до файлової системи
+- файл з кодом (App.java) повинен називатись так само (реєстрочутливо), як і клас описаний в ньому
+(class App)
+- файли впорядковуваються у каталоги, назва яких = назва пакету (package). Вимагається щонайменше
+подвійна вкладенність (group id) за принципом "інвесного домена", тобто коди фірми, що має сайт (домен)
+firm.org, будуть у пакетах org - firm - ...
+Є традиції іменування
+- types: CapitalCamelCase
+- names: lowerCamelCase
+- packages: snake_case
+- const: PASCAL_CASE (deprecated)
+
+
+ */
--- src/main/java/step/learning/Intro.java
+++ src/main/java/step/learning/Intro.java
@@ -1,2 +1,75 @@
-package step.learning;public class Intro {
+package step.learning;
+
+import java.util.*;
+
+public class Intro {
+    public void demo() {
+        System.out.println("Java intro");
+        byte b = -10;                // цілі числа різної размірності
+        short s = 1000;             // ! всі типи знакові, беззнакових - немає
+        int i = 1000000000;           //
+        long l = 1000000000000000000L;
+        float f = 0.1f;
+        double d = 1.23E-4;
+        char c = 'A';
+        boolean bb = true;
+        //System.out.println(bb);
+
+        int[] arr = new int[10];
+        int[][] arr2;    // jagged - "рвані" масиви - з можливою різною кількістю у підмасивах.
+
+        List<String> list1 = new ArrayList<>(); // <> - diamond operator
+        List<String> list2 = new LinkedList<>();
+        list1.add("Hello");    // String Pooling - компілятор веде перелік створених рядків
+        list2.add("Hello");    // і друга поява рядка посилається на першу.
+        if (list1.get(0).equals(list2.get(0))) {
+            System.out.println("Equals");;
+        } else {
+            System.out.println("Not equals");
+        }
+        list1.add("World");
+        list2.add("World !");
+
+
+        Map<Integer, String> map = new HashMap<>();   // ~Dictionary, Integer - boxing(int)
+        map.put(10, new String("Hello"));   // == reference equal - два посилання на один об'єкт
+                                                   // для порівняння контенту слід вживати str1.equals(str2)
+
+        map.put(11, "map World");
+
+        if (list1.get(0) == map.get(10)) {
+            System.out.println("Equals");;
+        } else {
+            System.out.println("Not equals");
+        }
+
+        for(String str : list1) {       // ~foreach
+            System.out.print(str + " ") ;
+        }
+        System.out.println(" ");
+        for(String str : list2) {       // ~foreach
+            System.out.print(str + " ") ;
+        }
+        System.out.println(" ");
+        for(Map.Entry<Integer, String> entry : map.entrySet()) {       // ~foreach
+            System.out.printf(
+                    "%d => %s, ",  // формат з плейсхолдеров
+                    entry.getKey(),  // значення для підстановки замість плейсхолдерів
+                    entry.getValue()  // {get, set} у Java немає, тому прийнято створювати аксесори
+            ) ;
+        }
+
+        // введеня даних
+        Scanner kbScanner = new Scanner(System.in);  // сканер вхідного потоку, запити на його
+        // читання зупиняють роботу та очікують введеня у консолі
+        System.out.print("\nEnter your name: ");
+        String name = kbScanner.nextLine(); // одне слово (~Console.Read)
+        System.out.println("Hello " + name);
+    }
 }
+/*
+Після створення нового проєкту необхідно налаштувати конфігурацію запуску (за замовченням
+запускається поточний файл).
+
+Типи даних - всі референсні за винятком примітивів.
+ */
